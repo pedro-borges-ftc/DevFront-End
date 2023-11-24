47bc9639f632585ebe140b6567e72d7d95ce8fe5
peuborg <peuborg.8@gmail.com>
2023-11-24 08:33:35 -0300

    fim aula

--- MeuPrimeiroProjeto/src/app/app.component.ts
+++ MeuPrimeiroProjeto/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from './components/hello/hello.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HelloComponent],
+  imports: [CommonModule, RouterOutlet, HelloComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ MeuPrimeiroProjeto/src/app/components/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
