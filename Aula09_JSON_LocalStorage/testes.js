c6c0e63fb9b7bb78b41841b1c1cbb8815abfd86f
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-11-14 20:22:18 -0300

    fim aula 09

---
+++ Aula09_JSON_LocalStorage/testes.js
@@ -0,0 +1,9 @@
+var aluno = {
+    "codigo":1,
+    "nome": "Fulano",
+    "email":"fulano@email.com"
+}
+console.log(aluno["nome"])
+aluno.nome = "Fulano de Talz"
+console.log(aluno.email)
+console.log(aluno)
