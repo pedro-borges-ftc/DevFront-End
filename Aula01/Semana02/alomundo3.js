a747fc877e7e6c4fa4b894e6c6c649eaebe2c4f3
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2025-02-06 20:15:21 -0300

    fim aula 02

--- Aula01/Semana02/alomundo3.js
+++ Aula01/Semana02/alomundo3.js
@@ -1,1 +1,12 @@
-window.alert("Esse é o bom e velho alôMundo3!")
+//window.alert("Esse é o bom e velho alôMundo3!")
+var algo = "ola mundo"
+const algoNovamente = "Novamente"
+
+function fazerAlgo() {
+    let algoNovo = "eaeh Galera"
+    window.alert(`1) Algo: ${algo} e algoNovo: ${algoNovo}`)
+}
+
+fazerAlgo()
+
+window.alert(`2) Algo: ${algo} e algoNovo: ${algoNovamente}`)
