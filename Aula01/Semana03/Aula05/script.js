cff5053df1344a65bfa04a2df5ce96fdd97fe0b9
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2025-02-13 21:06:29 -0300

    fim semana 3

---
+++ Aula01/Semana03/Aula05/script.js
@@ -0,0 +1,14 @@
+function corVermelho() {
+    var x = document.getElementById("demonstracao")
+    x.style.color = "red"
+}
+function corAzul() {
+    var x = document.getElementById("demonstracao")
+    x.style.color = "blue"
+}
+function corBody() {
+    var x = document.getElementById("bd")
+    x.style.backgroundColor = "gray"
+    var x = document.getElementById("demonstracao")
+    x.style.color = "white"
+}
---
+++ Aula02/01/script.js
@@ -0,0 +1,8 @@
+function modoDark() {
+    //atualizar body da página html
+    document.body.className = "dark";   
+}
+function modoWhite() {
+    //atualizar body da página html
+    document.body.className = "white";   
+}
