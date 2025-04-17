e6cd6f64adca9e89d352cfc5573b3fe1b2449961
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2025-04-17 20:53:11 -0300

    fim aula 05 template

--- Aula05_revisao/script.js
+++ Aula05_revisao/script.js
@@ -1,3 +1,45 @@
+function JurosSimples(){
+    //1) Leitura dos dados do HTML
+    //Comando para acessar o input html através do JS 
+    var txtvalorinicial = window.document.getElementById("txtvalorinicial").value
+    var txttaxa = window.document.getElementById("txtaxa").value
+    var txtperiodo = window.document.getElementById("txtperiodo").value    
+
+    //2) Processamento
+    var valorinicial = Number(txtvalorinicial)
+    var taxa = Number(txttaxa)
+    var periodo = Number(txtperiodo)
+    var juros = valorinicial * taxa / 100 * periodo
+    var total = valorinicial + juros
+
+    //3) Saída do resultado no HTML
+    var txtjuros = window.document.getElementById("txtjuros")
+    txtjuros.value = juros
+    var txttotal = window.document.getElementById("txttotal")
+    txttotal.value = total
+}
+
+function calcularIdade(){
+    //1) Leitura dos dados do HTML
+    //Comando para acessar o input html através do JS 
+    var dataNascimento = window.document.getElementById("txtdata").value
+    var resultado = window.document.getElementById("txtresultado")
+
+    //2) Processamento
+    var dtn = new Date(dataNascimento)
+    var hoje = new Date()
+    var idade = Math.floor(
+            Math.ceil(
+                Math.abs( dtn.getTime() - hoje.getTime()) 
+                / (1000 * 3600 * 24)
+                    ) / 365.25
+                )
+
+
+    //3) Saída do resultado no HTML
+    resultado.value = "A idade é " + idade + " ano(s)"
+}
+
 function somaNumeros(){
     //1) Leitura dos dados do HTML
     //Comando para acessar o input html através do JS 
